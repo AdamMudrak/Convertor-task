f2dad4922380e3d0108f4ecada35152dd3eaac67
AdamMudrak <mudrets2000@gmail.com>
2022-12-29 23:35:59 +0200

    Initial commit

---
+++ src/app/Main.java
@@ -0,0 +1,13 @@
+package app;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("""
+                Version 1.0
+                Hello! I convert kilometres
+                into miles and vice-versa.
+                What would you like to do today?
+                1 - convert miles into km
+                2 - convert km into miles""");
+    }
+}
