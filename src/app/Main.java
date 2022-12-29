05832159706783a0665b9ccf2cdeb5de6906c96a
AdamMudrak <mudrets2000@gmail.com>
2022-12-29 23:48:00 +0200

    Add method with scanner

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,13 +1,19 @@
 package app;
 
+import java.util.Scanner;
+
 public class Main {
+    static int menuOption;
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("""
-                Version 1.1
+                Version 1.2
                 Hello! I convert kilometres
                 into miles and vice-versa.
                 What would you like to do today?
                 1 - convert miles into km
                 2 - convert km into miles""");
+        menuOption = scanner.nextInt();
     }
 }
