d4197455d218998e8cb0fd72b0c5ea9f23277312
AdamMudrak <mudrets2000@gmail.com>
2022-12-29 23:50:36 +0200

    Add method kilometres to miles

--- src/app/Main.java
+++ src/app/Main.java
@@ -5,15 +5,24 @@ import java.util.Scanner;
 public class Main {
     static int menuOption;
 
+    static double kilometres;
+
     public static void main(String[] args) {
+        receiveInfo();
+    }
+
+    public static void receiveInfo() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("""
-                Version 1.2
+                Version 1.0
                 Hello! I convert kilometres
                 into miles and vice-versa.
                 What would you like to do today?
                 1 - convert miles into km
                 2 - convert km into miles""");
         menuOption = scanner.nextInt();
+    }
+    private static double convertKilometresToMiles(double kilometres) {
+        return kilometres / 1.60934;
     }
 }
