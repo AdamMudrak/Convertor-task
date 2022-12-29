a24bb9afd21b8c99cf376f6c00b9be9d741774e6
AdamMudrak <mudrets2000@gmail.com>
2022-12-29 23:55:43 +0200

    Add switch to enable menu

--- src/app/Main.java
+++ src/app/Main.java
@@ -23,9 +23,24 @@ public class Main {
                 1 - convert miles into km
                 2 - convert km into miles""");
         menuOption = scanner.nextInt();
+
+        switch (menuOption) {
+            case 1 -> {
+                System.out.println("Good! How many miles did you drive today?");
+                miles = scanner.nextDouble();
+            }
+            case 2 -> {
+                System.out.println("Good! How many kilometers did you drive today?");
+                kilometres = scanner.nextDouble();
+            }
+            default -> {
+                System.out.println("Wrong input! Try again...");
+                scanner.close();
+            }
+        }
     }
 
-    private static double convertMilesToKms(double miles) {
+    private static double convertMilesToKilometres(double miles) {
         return miles * 1.60934;
     }
 
